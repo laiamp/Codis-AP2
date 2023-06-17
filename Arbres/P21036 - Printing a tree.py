06240cec64db7ed4c5065abbbcae87be77ecf606
laiamp <81615713+laiamp@users.noreply.github.com>
2023-06-17 13:56:20 +0200

    Update P21036 - Printing a tree.py

--- Arbres/P21036 - Printing a tree.py
+++ Arbres/P21036 - Printing a tree.py
@@ -4,6 +4,7 @@ from typing import TypeAlias, Optional
 Bintree: TypeAlias = Optional[tuple[str, 'Bintree', 'Bintree']]
 
 def read_pre() -> Bintree:
+    '''llegim un arbre binari donat en preordre i el retornem'''
     s = read(str)
     if s == '-1':
         return None
@@ -12,15 +13,17 @@ def read_pre() -> Bintree:
 
 
 def print_bt(bt: Bintree, depth = 1) -> None:
-    # right, node, left
-    # tinc en compte la profunditat per imprimir els espais
+    '''imprimim un arbre binari tenint en compte els espais. 
+    Els espais depenen de la profunditat del node.
+    Recorrem l'arbre en inordre (començant per la dreta)'''
+    
     if bt is not None:
-        if bt[2] is not None:
+        if bt[2] is not None: # right
             print_bt(bt[2], depth + 1)
         
-        print(" "*(10*depth - len(bt[0]) - 1), bt[0])
+        print(" "*(10*depth - len(bt[0]) - 1), bt[0]) # node
 
-        if bt[1] is not None:
+        if bt[1] is not None: # left
             print_bt(bt[1], depth + 1)
 
 
@@ -28,4 +31,6 @@ def main() -> None:
     bt: Bintree = read_pre()
     print_bt(bt)
 
-main()
+
+if __name__ == "__main__":
+    main()
